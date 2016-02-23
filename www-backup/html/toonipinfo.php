1d3d4f5df35b3e21d80e3ec08680d64e4cf15df7
Zwijn <tom.tollenaere@student.ucll.be>
2016-02-23 19:12:09 +0100

    www folder gebackupd in etc

---
+++ www-backup/html/toonipinfo.php
@@ -0,0 +1,21 @@
+<?php
+
+//$output = shell_exec('/usr/bin/snmpwalk -v 2c -c public 192.168.1.2 1.3.6.1.4.1.14179.2.2.1.1.3| cut -d \'"\' -f2');
+//echo "<pre>$output</pre>";
+
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "POST"){
+	$ip = $_POST["IP"];
+}
+else{
+	$ip = $_GET["IP"];
+}
+exec("/etc/scripts/ToonIPInfo $ip", $output);
+
+
+foreach ($output as &$value) {
+   echo "$value";
+}
+
+
+?>
